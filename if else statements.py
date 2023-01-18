8a76c67789b79e632d5157a23a94a2079faadca2
Cyber-X-man <121112632+Cyber-X-man@users.noreply.github.com>
2023-01-18 21:35:07 +0600

    Add files via upload

---
+++ if else statements.py
@@ -0,0 +1,13 @@
+#if/elif/else statements.
+
+have_apple = 9
+if have_apple < 4:    #if statements
+    print("Low")
+    
+elif have_apple <= 5:  #elif statements
+    print('Medium')
+
+else:
+    print("High")       #else statements
+    
+
